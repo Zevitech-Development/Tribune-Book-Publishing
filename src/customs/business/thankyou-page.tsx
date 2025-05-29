74103e655d4b25b23ae9cc2425cfc7c3c901d8cd
Zevitech-Development <zevitechdevelopment@gmail.com>
2025-05-30 02:57:39 +0500

    fix build error

--- src/customs/business/thankyou-page.tsx
+++ src/customs/business/thankyou-page.tsx
@@ -11,13 +11,15 @@ function ThankyouPage() {
           <h1 className="lg:text-4xl md:text-3xl text-2xl !leading-[135%] text-heading font-libre font-bold">
             Thank you for getting in touch!
           </h1>
-          <h2 className="lg:text-xl md:text-lg text-base !leading-[135%] text-heading font-libre font-medium">Your request has been successfully submitted.</h2>
+          <h2 className="lg:text-xl md:text-lg text-base !leading-[135%] text-heading font-libre font-medium">
+            Your request has been successfully submitted.
+          </h2>
         </div>
       </div>
       <p className="lg:text-lg md:text-base text-sm text-center font-libre">
         One of our seasoned publishing experts will examine your information and
         contact you shortly to discuss your goals, answer your questions, and
-        offer customized support tailored to your book and vision. We're
+        offer customized support tailored to your book and vision. We&apos;re
         thrilled to assist you in taking the next step in your publishing
         journey with Tribune Book Publishing.
       </p>
